457a6fae3b4e562c1939f73fab0d778e29c37005
devvile <patryk.czemierowski@gmail.com>
2025-09-28 23:20:15 +0200

    linkedList class declaration

--- data structures/linked_list.js
+++ data structures/linked_list.js
@@ -1,13 +1,28 @@
-class ListNode {
+export class Node {
     constructor(val = 0, next = null) {
         this.val = val;
         this.next = next;
     }
 }
 
-class LinkedList{
+export class LinkedList{
     constructor(){
         this.head = null;
         this.size = 0;
     }
+
+    append(val){
+        const newNode = new Node(val);
+        if (!this.head){
+            this.head=newNode;
+            this.size++;
+        }else{
+            
+        }
+
+    }
 }
+
+const list = new LinkedList();
+list.append(10);
+console.log(list)
