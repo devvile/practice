7f3a456b92d71499ad5f1d4c493853d88e6141c0
devvile <patryk.czemierowski@gmail.com>
2025-09-28 22:20:02 +0200

    init

---
+++ data structures/linked_list.js
@@ -0,0 +1,13 @@
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+}
